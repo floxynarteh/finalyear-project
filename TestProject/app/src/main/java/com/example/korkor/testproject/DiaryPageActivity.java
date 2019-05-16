1ec9ab9b040b9f786e2d9ef704ba0208c98b20ab
Floxy Narteh <floxynarteh@gmail.com>
2019-05-16 19:57:32 +0000

    Worked on Note page

--- TestProject/app/src/main/java/com/example/korkor/testproject/DiaryPageActivity.java
+++ TestProject/app/src/main/java/com/example/korkor/testproject/DiaryPageActivity.java
@@ -1,59 +1,149 @@
 package com.example.korkor.testproject;
 
-import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.icu.text.SimpleDateFormat;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.DatePicker;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.Calendar;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class DiaryPageActivity extends AppCompatActivity {
 
-    private static final String TAG = "DiaryPageActivity";
-    private TextView mDisplayDate;
-    private DatePickerDialog.OnDateSetListener mDataSetListener;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference fNoteDatabase;
+    private EditText etNote, etTitle;
+    private Button btnSave;
+    private Toolbar myToolbar;
+    private Gson gson = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_page);
 
-        mDisplayDate =(TextView) findViewById(R.id.datePicker);
+        myToolbar = findViewById(R.id.m_toolbar);
+        btnSave = findViewById(R.id.save_button);
+        etNote = findViewById(R.id.note_content);
+        etTitle =  findViewById(R.id.note_title);
 
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        String data = getIntent().getStringExtra("notes");
+        NoteModel notes = gson.fromJson(data, NoteModel.class);
+        if (notes != null){
+            etNote.setText(notes.content);
+        }
+        setSupportActionBar(myToolbar);
+        mAuth = FirebaseAuth.getInstance();
 
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day= cal.get(Calendar.DAY_OF_MONTH);
+        fNoteDatabase = FirebaseDatabase.getInstance().getReference().child("Notes").child(mAuth.getCurrentUser().getUid());
 
-                DatePickerDialog dialog= new DatePickerDialog(
-                        DiaryPageActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDataSetListener,year,month,day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
-                dialog.show();
 
-            }
-        });
 
-        mDataSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                 month = month + 1;
-                 Log.d(TAG, "onDataSet; mm/dd/yyyy" + month + "/" + dayOfMonth + "/" + year);
-                 String date = month + "/" + dayOfMonth + "/" + year;
-                 mDisplayDate.setText(date);
-            }
-        };
+
 
     }
+
+    private void createNote(String title, String content) {
+
+        if (mAuth.getCurrentUser() != null) {
+            // creates random id notes
+            final DatabaseReference newNoteRef = fNoteDatabase.push();
+
+            new HashMap<String,String>().put(content,title);
+
+            final Map noteMap = new HashMap();
+            noteMap.put("title", title);
+            noteMap.put("content", content);
+            noteMap.put("timestamp", ServerValue.TIMESTAMP);
+
+            Thread mainThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    newNoteRef.setValue(noteMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(DiaryPageActivity.this, "Note added to database", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(DiaryPageActivity.this, " ERROR: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+
+
+            });
+            mainThread.start();
+
+
+        } else {
+            Toast.makeText(this, "User is not signed in", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.save_btn_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()) {
+            case R.id.save_button:
+                saveNoteModel();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+
+    private void saveNoteModel() {
+        String content = etNote.getText().toString().trim();
+        //NoteActivity noteModel= new NoteActivity(content);
+        String title = etNote.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title) || !TextUtils.isEmpty(content)) {
+            createNote(title, content);
+            finish();
+
+//        }else{
+//            Snackbar.make(view, "Fill empty field", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
